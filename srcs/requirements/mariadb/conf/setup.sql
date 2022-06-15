db7b66ff4865abd33ac59dd39863d6a292c9d63b
hportife <hportife@student.21-school.ru>
2022-06-15 17:08:59 +0300

    continue

--- srcs/requirements/mariadb/conf/setup.sql
+++
@@ -1,5 +0,0 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
-CREATE DATABASE wordpress;
-CREATE USER 'hportife'@'%' IDENTIFIED BY 'root';
-GRANT ALL ON wordpress.* TO 'hportife'@'%';
-FLUSH PRIVILEGES;
